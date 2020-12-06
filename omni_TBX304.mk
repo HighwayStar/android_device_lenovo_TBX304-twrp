c8f98a62e7dd87cbee44cf66010641208bca29e4
Vitaliy Tomin <highwaystar.ru@gmail.com>
2020-12-07 00:17:07 +0800

    TBX304: Rebrand to TB-X304 (Lenovo Tab 4 10)

--- omni_TBX304.mk
+++ omni_TBX304.mk
@@ -15,7 +15,7 @@
 #
 
 # Release name
-PRODUCT_RELEASE_NAME := TB8504
+PRODUCT_RELEASE_NAME := TBX304
 
 $(call inherit-product, build/target/product/embedded.mk)
 
@@ -23,19 +23,19 @@ $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lenovo/TB8504/device.mk)
+$(call inherit-product, device/lenovo/TBX304/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_TB8504
-PRODUCT_DEVICE := TB8504
+PRODUCT_NAME := omni_TBX304
+PRODUCT_DEVICE := TBX304
 PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := Lenovo TB-8504X
+PRODUCT_MODEL := Lenovo TB-X304X
 PRODUCT_MANUFACTURER := Lenovo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="msm8937_64-user 8.1.0 OPM1.171019.019 7 release-keys" \
-    TARGET_DEVICE="TB-8504X"
+    PRIVATE_BUILD_DESC="hq_msm8917-user 8.1.0 OPM1.171019.026 1418 release-keys" \
+    TARGET_DEVICE="TB-X304X"
 
-BUILD_FINGERPRINT := Lenovo/TB-8504X/TB-8504X:8.1.0/OPM1.171019.019/8504X_S001031_191204_ROW:user/release-keys
+BUILD_FINGERPRINT := Lenovo/LenovoTB-X304L/X304L:8.1.0/OPM1.171019.026/S001017_190709_ROW:user/release-keys
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo omni_TB8504-eng
-add_lunch_combo omni_TB8504-userdebug
+add_lunch_combo omni_TBX304-eng
+add_lunch_combo omni_TBX304-userdebug
